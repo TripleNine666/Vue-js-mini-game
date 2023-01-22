f920e2dc2a4d1b76f5a67d27ad3ca189b95b7606
TripleNine666 <kolumb1709@gmail.com>
2023-01-22 11:17:14 +0300

    health values and attack methods

---
+++ prj-monster-01-starting-setup/app.js
@@ -0,0 +1,25 @@
+function getRandomValue(minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonter() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+  },
+});
+
+app.mount("#game");
