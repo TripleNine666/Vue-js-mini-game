4cee9229343d1b02a0cd7ae9210e6ea66a22c64d
TripleNine666 <kolumb1709@gmail.com>
2023-01-24 23:54:55 +0300

    finish the game

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -9,13 +9,20 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   computed: {
     monsterHealthBar() {
+      if (this.monsterHealth <= 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerHealthBar() {
+      if (this.playerHealth <= 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     specialAttackAvailabil() {
@@ -50,17 +57,19 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
-
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "special attack", attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -71,7 +80,26 @@ const app = Vue.createApp({
       } else {
         this.playerHealth = 100;
       }
+      this.addLogMessage("player", "heal", healValue);
       this.attackPlayer();
+    },
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+      this.logMessages = [];
+    },
+    surrender() {
+      this.winner = "monster";
+      this.playerHealth = 0;
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
